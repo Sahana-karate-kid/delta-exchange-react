4fce241790615d9bc916462eb65891add8152fc5
Sahana-karate-kid <sahanakorlahalli@gmail.com>
2022-03-14 13:53:41 +0530

    New changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Row from './components/MemberRow.js';
+import Wss from './components/websocket';
+
+
 
 function App() {
+
+  
   return (
+      
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+             
+        <Row />
+       <Wss /> 
+      </div>
+       
+   );
 }
 
 export default App;
